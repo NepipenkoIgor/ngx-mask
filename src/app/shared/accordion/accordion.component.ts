5df7a03b71fe7fa22d70fbe5003e6c6ca9c99cad
NepipenkoIgor <igornepipenko@gmail.com>
2026-09-22 13:53:41 +0300

    chore(demo): remove redundant visit-website button

--- src/app/shared/accordion/accordion.component.ts
+++ src/app/shared/accordion/accordion.component.ts
@@ -13,7 +13,6 @@ import {
 import { DOCUMENT, isPlatformServer, NgOptimizedImage } from '@angular/common';
 import type { ListItem } from './content.types';
 import { AssetPipe } from '@shared/asset/asset.pipe';
-import { VisitBtnComponent } from '@shared/visit-btn/visit-btn.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BodyStylesService } from '@shared/body-styles/body-styles.service';
 import { OpenSourcePath } from '@shared/path/open-source.path';
@@ -23,7 +22,7 @@ import { AnchorLabelPipe } from '@shared/anchor/anchor-label.pipe';
     selector: 'ngxd-accordion',
     templateUrl: './accordion.component.html',
     styleUrl: './accordion.component.scss',
-    imports: [AssetPipe, VisitBtnComponent, NgOptimizedImage, AnchorLabelPipe],
+    imports: [AssetPipe, NgOptimizedImage, AnchorLabelPipe],
     standalone: true,
     providers: [BodyStylesService],
 })
--- src/app/shared/asset/asset.type.ts
+++ src/app/shared/asset/asset.type.ts
@@ -1,6 +1,2 @@
 export type AssetPath =
-    | 'shared'
-    | 'open-source/header'
-    | 'open-source/accordion'
-    | 'open-source/visit-btn'
-    | 'open-source/options';
+    'shared' | 'open-source/header' | 'open-source/accordion' | 'open-source/options';
